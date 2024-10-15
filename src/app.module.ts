06a8381c0e1de3ff742546cb0b602bd35e0a7005
HU YUTANG <huyutang@gmail.com>
2024-10-16 08:38:22 +0900

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,52 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PostsController } from './posts/posts.controller';
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { PostsModule } from "./modules/posts/posts.module";
+import { PostMiddleware } from "./core/middlewares/post/post.middleware";
+import { DemoAuthGuard } from "./core/guards/demo-auth.guard";
+import { DemoRolesGuard } from "./core/guards/demo-roles.guard";
+import { TransformInterceptor } from "./core/interceptors/transform/transform.interceptor";
+import { ErrorsInterceptor } from "./core/interceptors/errors/errors.interceptor";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, PostsController],
-  providers: [AppService],
+  imports: [PostsModule,
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "password",
+      database: "nestjs_db",
+      entities: [__dirname + "/**/*.entity{.ts,.js}"],
+      synchronize: true
+    }),
+    UserModule
+  ],
+  controllers: [AppController],
+  providers: [AppService,
+    {
+      provide: "APP_GUARD",
+      useClass: DemoAuthGuard
+    },
+    {
+      provide: "APP_GUARD",
+      useClass: DemoRolesGuard
+    }
+    // {
+    //   provide: "APP_INTERCEPTOR",
+    //   useClass: ErrorsInterceptor
+    // }
+  ]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    //
+    consumer
+      .apply(PostMiddleware)
+      .forRoutes("posts");
+  }
+
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DemoFilter } from "./core/filters/demo/demo.filter";
+import { ValidationPipe } from '@nestjs/common'
+import { LogginInterceptor } from "./core/interceptors/loggin/loggin.interceptor";
+import { TransformInterceptor } from "./core/interceptors/transform/transform.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //app.useGlobalFilters(new DemoFilter());
+  //app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalInterceptors(new LogginInterceptor(), new TransformInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
